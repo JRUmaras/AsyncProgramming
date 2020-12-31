cf592fda18f34c8127f125abd86617e62c2c10e5
Jonas Radvilas Umaras <jonas.umaras@nevda.lt>
2020-12-31 14:56:24 +0200

    Initial commit.

---
+++ App/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+
+namespace AsyncProgramming
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnExecSyncButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+
+            var stats = new WebSiteService().DownloadWebSiteStats();
+
+            var execTimeInSec = sw.ElapsedMilliseconds;
+
+            TbxResult.Text = $"{stats}\nExec SYNC took {execTimeInSec} s\n";
+        }
+
+        private async void OnExecAsyncButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+
+            var stats = await new WebSiteService().DownloadWebSiteStatsAsyncNonParallel();
+
+            var execTimeInSec = sw.ElapsedMilliseconds;
+
+            TbxResult.Text = $"{stats}\nExec ASYNC took {execTimeInSec} s\n";
+        }
+
+        private async void OnExecParallelAsyncButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+
+            var stats = await new WebSiteService().DownloadWebSiteStatsParallelAsync();
+
+            var execTimeInSec = sw.ElapsedMilliseconds;
+
+            TbxResult.Text = $"{stats}\nExec ASYNC took {execTimeInSec} s\n";
+        }
+    }
+}
---
+++ App/WebSiteService.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AsyncProgramming
+{
+    public class WebSiteService
+    {
+        private List<string> _webSiteUrls = new List<string>
+        {
+            "https://www.yahoo.com",
+            "https://www.google.com",
+            "https://www.microsoft.com",
+            "https://www.cnn.com",
+            "https://www.codeproject.com",
+            "https://www.stackoverflow.com",
+        };
+
+
+        public string DownloadWebSiteStats()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var url in _webSiteUrls)
+            {
+                var webSiteRawData = DownloadWebSite(url);
+                var webSiteStats = GetWebSiteStats(webSiteRawData);
+                sb.Append($"{url}: {webSiteStats}\n");
+            }
+
+            return sb.ToString();
+        }
+
+        public string DownloadWebSiteStatsParallel()
+        {
+            var sb = new StringBuilder();
+
+            Parallel.ForEach(_webSiteUrls, url =>
+            {
+                var webSiteRawData = DownloadWebSite(url);
+                var webSiteStats = GetWebSiteStats(webSiteRawData);
+
+                lock (sb) sb.Append($"{url}: {webSiteStats}\n");
+            });
+
+            return sb.ToString();
+        }
+
+        public Task<string> DownloadWebSiteStatsAsyncNonParallel()
+        {
+            return Task.Run(() => DownloadWebSiteStats());
+        }
+
+        public Task<string> DownloadWebSiteStatsParallelAsync()
+        {
+            return Task.Run(() => DownloadWebSiteStatsParallel());
+        }
+
+        private string DownloadWebSite(string url)
+        {
+            return new WebClient().DownloadString(url);
+        }
+
+        private string GetWebSiteStats(string webSiteRawData)
+        {
+            return $"Web site has {webSiteRawData.Length} characters.";
+        }
+    }
+}
